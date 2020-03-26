44a46bd61ed9efcd2a100780aaa2fab088dfda50
William <william.dower@griffithuni.edu.au>
2020-03-27 02:39:33 +1000

    New algorithm

--- main.cpp
+++ main.cpp
@@ -4,13 +4,38 @@
 #include <stack>
 #include <chrono>
 
+class node {
+public:
+    int branchPrime;
+    int amountLeft;
+    int degreesOfRemoval;
+    int posToSet;
+    bool posLocked;
+
+    node(int prime, int n) {
+        branchPrime = prime;
+        amountLeft = n;
+        degreesOfRemoval = 0;
+        posToSet = -1;
+        posLocked = false;
+    }
+
+    node(const node *oldNode, int prime) {
+        branchPrime = prime;
+        amountLeft = oldNode->amountLeft - prime;
+        degreesOfRemoval = oldNode->degreesOfRemoval + 1;
+        posToSet = oldNode->posToSet;
+        posLocked = oldNode->posLocked;
+    }
+};
+
 int* findPrimes(int max) {
     //0 means prime
     //1 means non-prime
     int *primeArr = new int [max+1]{};
 
     //All even numbers after 2 are not primes
-    for (int i=4;i<max;i+=2) {
+    for (int i=4;i<=max;i+=2) {
         primeArr[i] = 1;
     }
     //All odd numbers up to the square root of max is to be tested for primality
@@ -26,6 +51,170 @@ int* findPrimes(int max) {
     }
 
     return primeArr;
+}
+
+int newFindWays(int n, const int *primeArr, const int min, const int max) {
+    int ***waysArray = new int** [n+1];
+    waysArray[0] = nullptr; //No use for 0
+
+    for (int i=1;i<=n;i++) {
+        for (int j = i; j > 0; j--) { //Grab largest prime below amountLeft
+            //if (primeArr[j] == 0) {
+                waysArray[i] = new int* [j+1];
+                for (int k=1;k<=j;k++) {
+                    waysArray[i][k] = new int [i]{0};
+                }
+                break;
+            //}
+        }
+    }
+
+    std::stack<node*> stack;
+
+    for (int i=1;i<=n;i++) { //Main ways for each n loop
+
+        stack.push(new node(i, n));
+
+        while (!stack.empty()) {
+            node *currentNode = stack.top();
+            stack.pop();
+
+            for (int j=1;j<=currentNode->branchPrime;j++) { //Loop for n in prime j and under -- j is branchPrime
+                //Instead of referencing j for index, use posToSet, in case gone further down chain
+
+                if (primeArr[j] != 0) {
+                    continue;
+                }
+
+                if (currentNode->posToSet == -1) {
+                    currentNode->posToSet = j;
+                }
+                /**else if (!currentNode->posLocked) {
+                    currentNode->posToSet = j;
+                    currentNode->posLocked = true;
+                }**/
+
+                if (j == 1 && !currentNode->posLocked) { //Making n in 1 and under - always 1
+                    waysArray[i][i][i] = 1;
+                    if (!currentNode->posLocked) {
+                        currentNode->posToSet = -1;
+                        currentNode->posLocked = false;
+                    }
+                    continue;
+                }
+                else if (j == i && !currentNode->posLocked) { //Making n in in prime n and under - always 1
+                    waysArray[i][i][1] = 1;
+                    if (!currentNode->posLocked) {
+                        currentNode->posToSet = -1;
+                        currentNode->posLocked = false;
+                    }
+                    continue;
+                }
+
+                if (currentNode->amountLeft-j > j) { //Originally just > then added right half of or
+                    node *newNode = new node(currentNode, j);
+                    newNode->posLocked = true;
+                    stack.push(newNode);
+                    continue;
+                }
+                else {
+                    for (int k=1;k<=j;k++) {
+                        if (currentNode->posToSet > currentNode->amountLeft - j) {
+                            waysArray[i][currentNode->posToSet][k + currentNode->degreesOfRemoval + 1] =
+                                    waysArray[currentNode->amountLeft - j][currentNode->amountLeft - j][k];
+                        }
+                        else {
+                            waysArray[i][currentNode->posToSet][k + currentNode->degreesOfRemoval + 1] =
+                                    waysArray[currentNode->amountLeft - j][currentNode->posToSet][k];
+                        }
+                    }
+                }
+
+                currentNode->posToSet = -1;
+                currentNode->posLocked = false;
+            }
+
+        }
+    }
+
+    int ways = 0;
+    /**int largestPrimeBelowN = 0, ways = 0;
+    for (int i=n;i>0; i--) { //Grab largest prime below n
+        if (primeArr[i] == 0) {
+            largestPrimeBelowN = i;
+            break;
+        }
+    }**/
+
+    for (int i=1;i<=n;i++) {
+        /**if (primeArr[i] != 0) {
+            continue;
+        }**/
+        for (int j=min;j<=max;j++) {
+            ways += waysArray[n][i][j];
+        }
+    }
+
+    return ways;
+
+}
+
+int ripFindWays(int n, const int *primeArr) {
+    int ways = 0;
+    std::stack<node*> stack;
+    int **waysArray = new int*[n];
+    for (int i=0;i<n;i++) {
+        waysArray[i] = nullptr;
+    }
+
+    for (int i=1;i<n;i++) {
+        if (primeArr[i] == 1) {
+            continue;
+        }
+
+        stack.push(new node(i, n));
+
+        while (!stack.empty()) {
+            node *currentNode = stack.top();
+            stack.pop();
+            for (int j=1;j<currentNode->branchPrime;j++) {
+                if (primeArr[j] == 1) { //If j isn't prime, skip (only use prime j values)
+                    continue;
+                }
+                if (currentNode->branchPrime > currentNode->amountLeft) {
+                    for (int k=currentNode->amountLeft;k>0;k--) { //Grab largest prime below amountLeft
+                        if (primeArr[k] == 0) {
+                            currentNode->branchPrime = k;
+                            break;
+                        }
+                    }
+                }
+
+                if (currentNode->amountLeft > currentNode->branchPrime) {
+                    stack.push(new node(currentNode, j));
+                    continue;
+                }
+
+                if (waysArray[currentNode->amountLeft] != nullptr) {
+                    for (int k=0;k<=currentNode->branchPrime;k++) {
+                        if (k <= 2) {
+                            ways += 1;
+                        }
+                        else {
+                            ways += waysArray[currentNode->amountLeft][k-2];
+                        }
+                    }
+                }
+                else {
+
+                }
+
+            }
+            delete currentNode;
+        }
+
+    }
+
 }
 
 int findWays(int iters, int soFar, int max, int goalAmount, int *primeArr, int minIters, int maxIters) {
@@ -92,7 +281,9 @@ int main() {
 
     int *primeArr = findPrimes(amount);
 
-    std::cout << findWays(0, 0, amount, amount, primeArr, minIters, maxIters);
+    //std::cout << findWays(0, 0, amount, amount, primeArr, minIters, maxIters);
+    int ways = newFindWays(amount, primeArr, minIters, maxIters);
+    std::cout << ways << std::endl;
 
     auto endExecution = std::chrono::system_clock::now();
 
